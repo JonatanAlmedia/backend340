af66808bc12ea5526a708781265cb76f19745339
Jonatan Troche <alm19004@byui.edu>
2023-10-13 20:28:21 -0400

    new name

---
+++ database/assignment2.sql
@@ -0,0 +1,31 @@
+-- Task 1
+INSERT INTO account 
+	(account_firstname, account_lastname, account_email, account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+--Task 2
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+
+--Task 3
+DELETE FROM account
+WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
+
+--Task 4
+Update inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM';
+
+--Task 5
+SELECT inv_make, inv_model, classification_name
+FROM inventory i
+INNER JOIN classification c 
+ON i.classification_id = c.classification_id
+WHERE classification_name = 'Sport';
+
+-- Task 6
+UPDATE inventory
+SET
+	inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
